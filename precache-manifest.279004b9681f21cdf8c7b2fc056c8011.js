70bb6a413b82dcc4ff5c9f2e8f5b0cb892de2ef4
akshay sharma <akshay.3.1999@gmail.com>
2019-07-15 12:54:17 +0530

    Updates

---
+++ precache-manifest.279004b9681f21cdf8c7b2fc056c8011.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5f065432c778436c66c03b852c8747",
+    "url": "/emojiSearch/index.html"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/emojiSearch/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "250d041e60ea91c5f18f",
+    "url": "/emojiSearch/static/js/main.e5b3d861.chunk.js"
+  },
+  {
+    "revision": "409098e34972aad31be8",
+    "url": "/emojiSearch/static/js/runtime~main.916b5353.js"
+  }
+]);
